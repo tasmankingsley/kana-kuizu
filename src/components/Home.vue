6023dd5ddc40ed61d274324180907d9ea7c51fd3
tasmankingsley <tasman.kingsley@gmail.com>
2022-07-27 23:32:26 +0900

    refactor katakana object

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,13 +3,18 @@ import { ref } from 'vue'
 import { store } from './store'
 
 
+function start_quiz() {
+  store.kana_visible = !store.kana_visible
+}
+
+
 </script>
 
 <template>
   <h1>Kana Kuizu</h1>
 
   <div v-show="store.kana_visible">
-    <button type="button" @click="store.kana_visible = !store.kana_visible">start quiz</button>
+    <button type="button" @click="start_quiz">start quiz</button>
   </div>
 </template>
 
@@ -18,6 +23,12 @@ h1 {
   color: #2b2d42;
   font-weight: 200;
   font-size: 2.5rem;
+}
+
+h3 {
+  color: #2b2d42;
+  font-weight: 200;
+  font-size: 1.8rem;
 }
 
 </style>
